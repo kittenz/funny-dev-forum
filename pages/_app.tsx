ca949422b0eff309257e605e59915913a67201cc
Roni Nevalainen <kitten@kittenz.dev>
2021-05-24 18:02:53 +0300

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import type {AppProps} from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../src/styles/globals.scss';
+
+function MyApp({Component, pageProps}: AppProps) {
+	return <Component {...pageProps} />;
 }
-export default MyApp
+export default MyApp;
